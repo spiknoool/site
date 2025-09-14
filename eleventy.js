2eb65698a921e9e1aa17e1284c9f483531195e76
spiknoool <starsyskids@gmail.com>
2025-09-14 03:24:54 +0000

    Fix Eleventy build for GitHub Actions

--- eleventy.js
+++ eleventy.js
@@ -1,52 +1,41 @@
-// Tells Eleventy to look for Luxon
-const { DateTime } = require('luxon');
+// Import Luxon for date formatting
+const { DateTime } = require("luxon");
 
-// Tells Eleventy to look for the RSS plugin
+// Import Eleventy RSS plugin
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 
-// This is all the stuff that Eleventy is going to process when it exports your site
-module.exports = function (eleventyConfig) {
-  // Edit these to include your images, CSS, and other folders and files that you want to copy over to your public folder.
-  eleventyConfig.addPassthroughCopy("./src/styles");
-  eleventyConfig.addWatchTarget("./src/styles/");
-  eleventyConfig.addPassthroughCopy("./src/images");
-  eleventyConfig.addWatchTarget("./src/images/");
+module.exports = function(eleventyConfig) {
+  // Passthroughs for static assets
+  eleventyConfig.addPassthroughCopy("src/styles");
+  eleventyConfig.addPassthroughCopy("src/images");
 
-  // Load the RSS plugin
+  // RSS plugin
   eleventyConfig.addPlugin(pluginRss);
 
-  // Adds Next & Previous links to the bottom of our blog posts
+  // Blog post collection with prev/next links
   eleventyConfig.addCollection("posts", function(collection) {
-    const coll = collection.getFilteredByTag("posts");    
-    for(let i = 0; i < coll.length ; i++) {
-      const prevPost = coll[i-1];
-      const nextPost = coll[i + 1];      
-      coll[i].data["prevPost"] = prevPost;
-      coll[i].data["nextPost"] = nextPost;
+    const posts = collection.getFilteredByTag("posts");
+    for (let i = 0; i < posts.length; i++) {
+      posts[i].data.prevPost = posts[i - 1] || null;
+      posts[i].data.nextPost = posts[i + 1] || null;
     }
-    return coll;
+    return posts;
   });
 
-  // Add the filter "readableDate" to simplify the way blog dates are presented in the Archives page
-  eleventyConfig.addFilter('readableDate', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, { zone: 'utc+9' }).toFormat(
-      'yyyy-LL-dd'
-    );
+  // Date filters
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc+9" }).toFormat("yyyy-LL-dd");
   });
 
-  // Add the filter "topDate" to simplify the way blog dates are presented at the top of blog posts
-  eleventyConfig.addFilter('topDate', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, { zone: 'utc+9' }).toFormat(
-      'yyyy LLLL dd'
-    );
+  eleventyConfig.addFilter("topDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc+9" }).toFormat("yyyy LLLL dd");
   });
 
-  // These are the folders that Eleventy will use. "src" is where you edit files that Eleventy will then take in and export into "public," which you upload.
-    return {
-      dir: {
-        input: "src",
-        output: "public",
-      },
-    };
+  // Eleventy directory config
+  return {
+    dir: {
+      input: "src",   // source folder
+      output: "public" // output folder matches workflow
+    }
   };
-  
+};
